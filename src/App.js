82730f142504a3034a280aa15c6b8f82ff024d29
Martinho Santos <mbs@particleForward.com>
2019-05-16 19:28:50 +0300

    deploy #1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacterino feature-test v1
+          Learn Reacterino feature-testeroo v1
         </a>
       </header>
     </div>
