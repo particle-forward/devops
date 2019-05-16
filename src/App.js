43c4b18fb6b4c57c63f9ca2f9b1fdeb6586ee0e6
Martinho Santos <mbs@particleForward.com>
2019-05-16 22:55:41 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacterino ci-deployeroo v1
+          Learn Reacterino ci-deployeroo v2
         </a>
       </header>
     </div>
