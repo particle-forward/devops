924fdf18f8724f029b7c1df83b682e1000cbec5b
Martinho Santos <mbs@particleForward.com>
2019-05-16 19:19:45 +0300

    build #14

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacterino v9
+          Learn Reacterino feature-test v1
         </a>
       </header>
     </div>
