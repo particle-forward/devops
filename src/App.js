4e8efae0277f2868a8c894b049e4106d2d20db1a
Martinho Santos <mbs@particleForward.com>
2019-05-17 10:12:00 +0300

    feature-ci-testing v1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacterino ci-deployeroo v2
+          Learn Reacterino feature-ci-testing v1
         </a>
       </header>
     </div>
